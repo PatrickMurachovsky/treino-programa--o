077babb185b9207ae5361e1b82b8d82bb0fec213
PatrickMurachovsky <patrick.murachovsky@sou.inteli.edu.br>
2025-02-11 11:35:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let nota1 = 7
+let nota2 = 5
+let nota3 = 6
+
+let mediageral = (nota1+nota2+nota3) / 3
+if (mediageral >= 7){
+    console.log ('Voce esta aprovado')
+
+    
+}
+else(mediageral <=7)
+    console.log ('Voce esta reprovado')
+
+
